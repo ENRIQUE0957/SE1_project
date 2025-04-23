b8bb73d18a8f6a8887980a37c3e0e255866e8d77
JOSHUA BULLOCK <1JOSHEB1@GMAIL.COM>
2025-04-23 00:01:57 -0500

    commit

--- SE1_Project/public/view/progress_message.js
+++
@@ -1,5 +0,0 @@
-export function progressMessage(message){
-    const div = document.createElement('div');
-    div.innerHTML = `<h4 class="text-white bg-success">${message}</h4>`;
-    return div;
-}
